92128e4aaf6ca8924d53ac3d55a2c7eb541ecc6d
Chris Tsai <tsai_cheng@yahoo.com>
2019-09-05 16:02:49 +0800

    apply middleware

--- frontend/store/store.js
+++ frontend/store/store.js
@@ -1,7 +1,15 @@
-import { createStore, applyMiddleware } from 'redux';
+import {
+  createStore,
+  applyMiddleware
+} from 'redux';
 import logger from 'redux-logger';
-import rootReducer from '../reducers/root_reducer';
 
-const configureStore = () => createStore(rootReducer, applyMiddleware(logger));
+import rootReducer from '../reducers/root_reducer';
+import thunk from '../middleware/thunk';
+
+const configureStore = () => createStore(
+  rootReducer,
+  applyMiddleware(thunk, logger)
+);
 
 export default configureStore;
