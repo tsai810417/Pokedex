916f13e62542b9ba55e3ba01d28de29da5f08a55
Chris Tsai <tsai_cheng@yahoo.com>
2019-09-05 21:53:35 +0800

    handle receiveSinglePokemon action

--- frontend/reducers/items_reducer.js
+++ frontend/reducers/items_reducer.js
@@ -1,6 +1,13 @@
+import {
+  RECEIVE_SINGLE_POKEMON
+} from '../actions/pokemon_actions';
+
 const itemsReducer = (state = {}, action) => {
   Object.freeze(state);
+
   switch (action.type) {
+    case RECEIVE_SINGLE_POKEMON:
+      return Object.assign({}, state, action.payload.items);
     default:
       return state;
   }
