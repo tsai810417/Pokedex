80819b9cbb5f34c57fd8ff55f6f8392578c044e1
Chris Tsai <tsai_cheng@yahoo.com>
2019-09-05 21:54:15 +0800

    craete requestSinglePokemon thunk action

--- frontend/actions/pokemon_actions.js
+++ frontend/actions/pokemon_actions.js
@@ -3,6 +3,7 @@ import * as APIUtil from '../util/api_util';
 export const RECEIVE_ALL_POKEMON = 'RECEIVE_ALL_POKEMON';
 export const REQUEST_ALL_POKEMON = 'REQUEST_ALL_POKEMON';
 export const RECEIVE_SINGLE_POKEMON = 'RECEIVE_SINGLE_POKEMON';
+export const REQUEST_SINGLE_POKEMON = 'REQUEST_SINGLE_POKEMON';
 
 export const receiveAllPokemon = pokemon => ({
   type: RECEIVE_ALL_POKEMON,
@@ -17,4 +18,9 @@ export const receiveSinglePokemon = payload => ({
 export const requestAllPokemon = () => dispatch => (
   APIUtil.fetchAllPokemon()
     .then(pokemon => dispatch(receiveAllPokemon(pokemon)))
+)
+
+export const requestSinglePokemon = id => dispatch => (
+  APIUtil.fetchSinglePokemon(id)
+    .then(pokemon => dispatch(receiveSinglePokemon(pokemon)))
 )
