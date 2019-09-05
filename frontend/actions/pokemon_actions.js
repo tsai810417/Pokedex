a1e0804a80804d052742894bedb7304f1900a727
Chris Tsai <tsai_cheng@yahoo.com>
2019-09-05 21:50:21 +0800

    create receiveSinglePokemon action

--- frontend/actions/pokemon_actions.js
+++ frontend/actions/pokemon_actions.js
@@ -2,10 +2,16 @@ import * as APIUtil from '../util/api_util';
 
 export const RECEIVE_ALL_POKEMON = 'RECEIVE_ALL_POKEMON';
 export const REQUEST_ALL_POKEMON = 'REQUEST_ALL_POKEMON';
+export const RECEIVE_SINGLE_POKEMON = 'RECEIVE_SINGLE_POKEMON';
 
 export const receiveAllPokemon = pokemon => ({
   type: RECEIVE_ALL_POKEMON,
   pokemon
+});
+
+export const receiveSinglePokemon = payload => ({
+  type: RECEIVE_SINGLE_POKEMON,
+  payload
 });
 
 export const requestAllPokemon = () => dispatch => (
