3a220475a6393b16dab1bc1cb17866773d344c66
Chris Tsai <tsai_cheng@yahoo.com>
2019-09-05 15:56:51 +0800

    set up store at entry file

--- frontend/pokedex.jsx
+++ frontend/pokedex.jsx
@@ -1,7 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import configureStore from './store/store';
 
 document.addEventListener('DOMContentLoaded', () => {
   const rootEl = document.getElementById('root');
+  const store = configureStore();
+
   ReactDOM.render(<h1>Pokedex</h1>, rootEl);
 });
