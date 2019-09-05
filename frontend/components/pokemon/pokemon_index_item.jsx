1349e022595159c02b1a1e9cd7978c4f0100d6ec
Chris Tsai <tsai_cheng@yahoo.com>
2019-09-05 17:53:33 +0800

    route each pokemonIndexItem to its link

--- frontend/components/pokemon/pokemon_index_item.jsx
+++ frontend/components/pokemon/pokemon_index_item.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
+import { Link } from "react-router-dom";
 
 export const PokemonIndexItem = ({ pokemon }) => (
   <li>
-    <img src={pokemon.image_url} alt={pokemon.name} style={{width: '30px', height: '30px'}}/>
-    <span>{ pokemon.name }</span>
+    <Link to={`/pokemon/${ pokemon.id }`}>
+      <img src={ pokemon.image_url } alt={pokemon.name} style={{width: '30px', height: '30px'}}/>
+      <span>{ pokemon.name }</span>
+    </Link>
   </li>
 );
